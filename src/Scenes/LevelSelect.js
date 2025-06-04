58a316ca36c0cf62b4e650077128879460217abc
ZanderZ15 <zandermillerbrown@gmail.com>
2025-06-03 19:29:18 -0700

    Added level select?

--- src/Scenes/LevelSelect.js
+++ src/Scenes/LevelSelect.js
@@ -1,15 +1,16 @@
-class Title extends Phaser.Scene {
+class Select extends Phaser.Scene {
     constructor() {
-        super("title");
+        super("select");
         this.my = {text: {}};
     }
     create() {
         let my = this.my;
         
         const width = 1520
-        my.text.wt = this.add.bitmapText(width/2, 120, "rocketSquare", "Leap!").setOrigin(.5, 0); 
-        my.text.ze = this.add.bitmapText(width/2, 180, "rocketSquare", "Frog?").setOrigin(.5, 0);
-        my.text.ptp = this.add.bitmapText(width/2, 520, "rocketSquare", "Press SPACE to play").setOrigin(.5, 0);
+        my.text.a = this.add.bitmapText(width/2, 120, "rocketSquare", "Press Space to go to \"Level1\"").setOrigin(.5, 0); 
+        my.text.b = this.add.bitmapText(width/2, 160, "rocketSquare", "Press M to go to \"mountains\"").setOrigin(.5, 0);
+        my.text.c = this.add.bitmapText(width/2, 200, "rocketSquare", "Press N to go to \"Mushrooms\"").setOrigin(.5, 0);
+        my.text.d = this.add.bitmapText(width/2, 240, "rocketSquare", "Press 0 to go to \"volcano\"").setOrigin(.5, 0);
         this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.zeroKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ZERO);
         this.mKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M);
@@ -17,16 +18,16 @@ class Title extends Phaser.Scene {
     }
     update() {
         if (Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
-            this.scene.start("select");
-        }
-        if (Phaser.Input.Keyboard.JustDown(this.zeroKey)) {
-            this.scene.start("volcano");
+            this.scene.start("level1");
         }
         if (Phaser.Input.Keyboard.JustDown(this.mKey)) {
             this.scene.start("mountain");
         }
         if (Phaser.Input.Keyboard.JustDown(this.nKey)) {
             this.scene.start("mushroomlevel");
+        }
+        if (Phaser.Input.Keyboard.JustDown(this.zeroKey)) {
+            this.scene.start("volcano");
         }
     }
 }
